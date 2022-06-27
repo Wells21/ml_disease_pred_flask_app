aa51db9664fb5c16e49096fe6770217323671596
Wellspring Praise <wellspringpraise@gmail.com>
2022-06-27 13:43:55 +0100

    initial commit

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from logging import raiseExceptions
+from flask import Flask, render_template, request
+from joblib import load
+import numpy as np
+
+
+app = Flask(__name__)
+
+model = load('disease_model.joblib')
+
+
+@app.route("/")
+def hello_world():
+        return render_template('index.html')
+
+@app.route("/predict", methods = ["POST"])
+def predict():
+        int_inputs = [int(x) for x in request.form.values()]
+        inputs = [np.array(int_inputs)]
+        preds = model.predict(inputs)
+
+
+        return render_template('index.html', prediction_text = "The Disease is: {}".format(preds))
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
